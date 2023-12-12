fffe3cc636a0dd5ee3fe95fc48500f8a523dfd65
ivanFlor3s <ifloresmanc96@gmail.com>
2023-12-12 01:23:08 -0300

    fail, same commit

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -20,14 +20,15 @@ export const getAll = (req = request, res = response) => {
     let promise = !!filter
         ? User.findAll({
               where: {
-                [Op.or]:[
-                    {name: { [Op.iLike]: `%${filter}%` }},
-                    {lastName: { [Op.iLike]: `%${filter}%` }},
-                    {email: { [Op.iLike]: `%${filter}%` }},
-                ]
+                  [Op.or]: [{ name: { [Op.iLike]: `%${filter}%` } }, { lastName: { [Op.iLike]: `%${filter}%` } }, { email: { [Op.iLike]: `%${filter}%` } }],
+                  id: { [Op.ne]: req.usuario.id },
               },
           })
-        : User.findAll();
+        : User.findAll({
+                where: {
+                    id: { [Op.ne]: req.usuario.id },
+                },
+        });
 
     promise
         .then((users) => {
@@ -38,3 +39,26 @@ export const getAll = (req = request, res = response) => {
             res.status(500).send(new ApiResponse(null, error.message));
         });
 };
+
+export const deleteUser = async (req = request, res = response) => {
+    const userId = req.params.id;
+    try {
+        await User.destroy({ where: { id: userId } });
+        res.status(200).send(new ApiResponse(null, 'Se elimino el usuario'));
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(new ApiResponse(null, error.message));
+    }
+};
+
+export const changeRol = async (req = request, res = response) => {
+    const userId = req.params.id;
+    const { role } = req.body;
+    try {
+        await User.update({ role }, { where: { id: userId } });
+        res.status(200).send(new ApiResponse(null, 'Se cambio el rol del usuario'));
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(new ApiResponse(null, error.message));
+    }
+}
