f2c73e79e1b13e1d4b402dacfa3a9f0390920faf
ivanFlor3s <ifloresmanc96@gmail.com>
2023-12-17 12:35:50 -0300

    game api, create scores

---
+++ src/api/game.api.js
@@ -0,0 +1,18 @@
+import { Game } from "../models/game.js";
+import { Score } from "../models/score.js";
+
+export class GameApi {
+  create = async ({users}) => {
+    try {
+      const currentGame = await Game.findOne({ where: { inProgress: true } });
+      const scores = users.map(user => ({UserId: user.userId, GameId: currentGame.dataValues.id, score: user.puntaje }))
+      await Score.bulkCreate(scores)
+      return data;
+    } catch (error) {
+      return error;
+    }
+  };
+  
+   
+}
+
